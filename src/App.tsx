33a3c31a10b3fe5e3b97b09bc38ba8b3d2357cd6
lyubov08 <lyubovvoronchikhina@gmail.com>
2025-11-28 15:07:52 +0500

    Добавлена страница Gallery с фильтрацией по художникам и ориентации, 3 картины в строке

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
-// import Gallery from './pages/Gallery/Gallery';
+import Gallery from './pages/Gallery/Gallery';
 // import Author from './pages/Author/Author';
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/gallery" element={<Gallery />} />
-        <Route path="/author" element={<Author />} /> */}
+        <Route path="/gallery" element={<Gallery />} />
+        {/* <Route path="/author" element={<Author />} /> */}
       </Routes>
     </Router>
   );
